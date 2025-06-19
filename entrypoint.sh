4c5b0170fda53a09b7341952ba87ea08aad0cd61
Meet Hatkar <110737691+meethatkar@users.noreply.github.com>
2025-06-19 23:30:04 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,14 @@
 
 echo "========================="
 
-git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.name "${INPUT_NAME}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
 
 git add -A && git commit -m"Update Feed"
-
+git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 git push --set-upstream origin main 
 
 echo "========================="
